b29a5c9791ce71936b53b28dc4b8a1d9943ae4b2
Jan Keromnes <janx@linux.com>
2019-03-18 10:10:17 +0100

    [mobile] implemented mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import styled from '@emotion/styled'
 import { transparentize } from 'polished'
 import { Link } from 'gatsby'
 
-import { heights, dimensions, colors } from '../styles/variables'
+import { heights, dimensions, colors, breakpoints } from '../styles/variables'
+import { getEmSize } from '../styles/mixins'
 import Container from './Container'
 import logo from '../resources/Logo_Gitpod.svg';
+import * as icons from '../resources/icons';
 
 const StyledHeader = styled.header`
   height: ${heights.header}px;
@@ -13,29 +15,88 @@ const StyledHeader = styled.header`
   margin-top: 15px;
   background-color: ${colors.background1};
   color: ${transparentize(0.5, colors.fontColor1)};
+
+  @media (max-width: ${getEmSize(breakpoints.md)}em) {
+    z-index: 2;
+    position: fixed;
+    width: 100%;
+    padding: 0;
+    padding-top: 5px;
+    margin: 0;
+  }
 `
 
 const HeaderInner = styled(Container)`
     display: flex;
     flex-direction: row;
     align-items: center;
+    flex-wrap: wrap;
     height: 100%;
-    justify-content: space-between;
+    padding: 0 ${dimensions.containerPadding}rem;
+    z-index: 2;
 `
 
-const HeaderLeft = styled.div`
+const MobileMenu = styled.div`
+  margin-left: auto;
+
+  > :not(:last-child) {
+    padding-right: 15px;
+  }
+
+  @media (min-width: ${getEmSize(breakpoints.md)}em) {
+    display: none;
+  }
+`
+
+const Menu = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  flex-grow: 1;
   height: 100%;
-`
 
-const HeaderRight = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-  height: 100%;
+  > :last-child {
+    margin-left: auto;
+  }
+
+  @media (max-width: ${getEmSize(breakpoints.md)}em) {
+    height: auto;
+    width: 100%;
+    margin-top: 15px;
+    flex-direction: column;
+
+    :before {
+      content: '';
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${colors.background1};
+      z-index: -1;
+    }
+
+    :not(.open) {
+      display: none;
+    }
+
+    > * {
+      padding: 15px;
+      width: 100%;
+    }
+
+    > *:not(:last-child) {
+      border-bottom: 1px solid ${colors.background3};
+    }
+
+    > :last-child {
+      margin: 0;
+
+      > button {
+        width: 100%;
+      }
+    }
+  }
 `
 
 const HomepageLink = styled(Link)`
@@ -54,23 +115,43 @@ interface HeaderProps {
     title: string
 }
 
-const Header: React.SFC<HeaderProps> = () => (
-    <StyledHeader>
-        <HeaderInner>
-            <HeaderLeft>
-                <HomepageLink to="/" style={{ paddingRight: 60 }}><img src={logo} style={{ width: 120 }} /></HomepageLink>
-                <HomepageLink to="/features">Features</HomepageLink>
-                <HomepageLink to="/pricing">Pricing</HomepageLink>
-                <HomepageLink to="/docs">Docs</HomepageLink>
-                <HomepageLink to="/blog">Blog</HomepageLink>
-            </HeaderLeft>
-            <HeaderRight>
-                <a href="https://gitpod.io/api/login">
-                    <button className='primary'>Log In</button>
-                </a>
-            </HeaderRight>
-        </HeaderInner>
-    </StyledHeader>
-)
+export default class Header extends React.Component<HeaderProps, {}> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            isMenuOpen: false
+        }
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
 
-export default Header
+    toggleMenu() {
+        const { isMenuOpen } = this.state;
+        this.setState({ isMenuOpen: !isMenuOpen })
+    }
+
+    render() {
+        const { isMenuOpen } = this.state;
+        return <StyledHeader>
+            <HeaderInner>
+                <HomepageLink to="/" style={{ paddingRight: 60 }}><img src={logo} style={{ width: 120 }} /></HomepageLink>
+                <MobileMenu>
+                    {isMenuOpen ? null : <a href="https://gitpod.io/api/login">
+                        <button className='primary'>Log In</button>
+                    </a>}
+                    <button onClick={this.toggleMenu} style={{border: 0}}>
+                        {isMenuOpen ? icons.cross() : icons.burger()}
+                    </button>
+                </MobileMenu>
+                <Menu className={isMenuOpen ? 'open' : ''}>
+                    <HomepageLink to="/features">Features</HomepageLink>
+                    <HomepageLink to="/pricing">Pricing</HomepageLink>
+                    <HomepageLink to="/docs">Docs</HomepageLink>
+                    <HomepageLink to="/blog">Blog</HomepageLink>
+                    <a href="https://gitpod.io/api/login">
+                        <button className='primary'>Log In</button>
+                    </a>
+                </Menu>
+            </HeaderInner>
+        </StyledHeader>;
+    }
+}
