cf776ba8dc9fb8ce2867a67499ddb01d4b6b6b93
Jan Keromnes <jan.keromnes@typefox.io>
2019-04-04 06:52:18 +0000

    [logos] Don't flash white alt text on page loads

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -10,7 +10,7 @@ const Logos: React.SFC<LogosProps> = ({ logos }) => (
     <>
         {logos.map(logoPlacement => {
             const [left, top, width] = logoPlacement;
-            return <img alt='Gitpod Logo' key={''+left+top+width} src={greyLogo} className='hidden-md-down' style={{
+            return <img aria-hidden='true' key={''+left+top+width} src={greyLogo} className='hidden-md-down' style={{
                 position: 'absolute',
                 left,
                 top,
