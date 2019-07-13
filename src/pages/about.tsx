e278b0256885d20f34ed03a8a667135aa79fe1df
nisarhassan12 <syednisarhassan12@gmail.com>
2019-07-13 05:30:18 +0000

    Created the About Page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,12 +10,20 @@ import styled from '@emotion/styled'
 import { getEmSize } from '../styles/mixins'
 import { breakpoints } from '../styles/variables'
 import { Link } from 'gatsby'
-import KeilImage from '../resources/placeholder.jpg'
+import Team from '../resources/team.jpg'
+import KeilHafen from '../resources/Kiel_hafen-compressor.svg'
 import IBM from '../resources/ibm.svg'
+import ARM from '../resources/2880px-Arm_logo_2017.svg'
+import Bosch from '../resources/bosch.svg'
+import Eclipse from '../resources/eclipse.svg'
+import Ericsson from '../resources/ericsson.svg'
+import GoogleCloud from '../resources/google-cloud.svg'
+import Sap from '../resources/sap.svg'
+import Redhat from '../resources/redhat.svg'
 
 const About = styled.div`
     display: flex;
-    margin: 80px 0 100px 0;
+    margin: 100px 0 120px 0;
 
     @media(max-width: ${getEmSize(breakpoints.md - 1)}em) {
         flex-direction: column;
@@ -53,6 +61,7 @@ const About = styled.div`
 
     .text-container {
         width: 75%;
+
         p + p {
             margin-top: 40px;
         }
@@ -68,6 +77,7 @@ const Description = styled.div`
     }
 
     div {
+        position: relative;
         width: 45%;
 
         @media(max-width: ${getEmSize(breakpoints.md - 1)}em) {
@@ -75,32 +85,50 @@ const Description = styled.div`
             margin: 0 auto;
         }
 
-        &:not(:last-child) {
-            margin-right: 10%;
+        @media(min-width: ${getEmSize(breakpoints.md)}em) {
+             &:not(:last-child) {
+                margin-right: 15%;
+            }
         }
     }
 
     .img-container {
         width: 300px;
-        height: 200px;
+        height: auto
         display: block;
         margin: 50px auto 70px;
         border: 1px solid #eee;
+
+        &--hafen {
+            width: 195px;
+            transform: translateY(-40px) translateX(-70px);
+        }
+    }
+
+    .para {
+        transform: translateY(-47px);
     }
 
     img {
-       transform: translate(-20px, 20px);
+        width: 300px;
+        transform: translate(-14px, 16px);
+    }
+
+    .hafen {
+        height: 250px;
+        transform: translate(-16px, 20px);
     }
 `
 
 const Timeline = styled.div`
     margin: 50px 0 100px 0;
+    transform: translateX(-85px);
 
     & > div {
         display: flex;
 
         div {
-            padding: 50px;
+            padding: 35px 80px;
             font-size: 18px;
         }
     }
@@ -111,7 +139,7 @@ const Timeline = styled.div`
 
         &::after {
             position absolute;
-            right: -6%;
+            right: -4.5%;
             top: 50%;
             transform: translateY(-50%);
             content: '';
@@ -130,6 +158,45 @@ const Timeline = styled.div`
             font-weight: normal;
         }
     }
+
+    .last {
+        position: relative;
+        &::after {
+            position: absolute;
+            top: 2;
+            left: 17%;
+            content: '';
+            transform: translateY(60%);
+            display: block;
+            height: 100%;
+            width: 8%;
+            background: ${colors.background1};
+
+            @media(max-width: 850px) {
+                transform: translateY(57%);
+            }
+        }
+    }
+
+    .since {
+        color: #797979;
+
+        .date {
+            span {
+                position: relative;
+                color: transparent;
+
+                &::before {
+                    content: "Since";
+                    position: absolute;
+                    left: -22px;
+                    display: inline-block;
+                    color: #797979;
+                }
+            }
+        }
+    }
+
 `
 
 const Partners = styled.div`
@@ -142,9 +209,13 @@ const Partners = styled.div`
         text-align: center;
         width: 50%;
         @media(min-width: ${getEmSize(breakpoints.md)}em) {
-            text-align: left;
             width: 25%;
         }
+    }
+
+    img {
+        height: 80px;
+        width: 80px;
     }
 `
 
@@ -152,51 +223,52 @@ const AboutPage: React.SFC<{}> = () => (
     <IndexLayout title="Gitpod - About" canonical="https://www.gitpod.io/about">
         <Page>
             <Container>
-               <div style={{ marginTop: 100 }}>
+                <div style={{ marginTop: 120 }}>
                     <h4 style={{ color: colors.fontColor2 }}>About us</h4>
                     <h1>The Company behind Gitpod</h1>
                 </div>
                 <About>
                     <div className='logo-container'>
-                        <img  src={TypefoxLogo} alt="Typefox Logo"/>
-                        <div className="logo-text">Type<span style={{fontWeight: 'bolder'}}>Fox</span></div>
+                        <img src={TypefoxLogo} alt="Typefox Logo" />
+                        <div className="logo-text">Type<span style={{ fontWeight: 'bolder' }}>Fox</span></div>
                     </div>
                     <div className="text-container">
                         <p>
-                            Gitpod is developed Typefox, a team of developer tool specialists and longtime contributors to many open-source projects. We are a driving force behind the language server protocol and the initiators of Eclipse Theia, the next generation IDE that powers Gitpod.
+                            Gitpod is developed <a href="https://typefox.io" target="_blank" style={{ textDecoration: 'underline' }}>TypeFox</a>, a team of developer tool specialists and longtime contributors to many open-source projects. We are a driving force behind the language server protocol and the initiators of Eclipse Theia, the next generation IDE that powers Gitpod.
                         </p>
                         <p>
-                            Open-source projects like Kubernetes, Theia, Typescript, VS Code and thousands of other libraries are the gaint shoulders Gitpod is standing on. The typefox team is happy and truly thankful to live in a time where we can participate in such lively and creative communties with so many cool ideas and so much passion. Without those, developing Gitpod would not have been possible.
+                            Open-source projects like Kubernetes, Theia, TypeScript, VS Code and thousands of other libraries are the giants' shoulders Gitpod is standing on. The TypeFox team is happy and truly thankful to live in a time where we can participate in such lively and creative communties with so many cool ideas and so much passion. Without those, developing Gitpod would not have been possible.
                         </p>
                     </div>
                 </About>
             </Container>
             <Container>
-                <h2>From developers for developers</h2>
+                <h2>By developers for developers</h2>
                 <Description>
                     <div>
-                        <div className="img-container">
-                            <img src={KeilImage}/>
+                        <div className="img-container img-container--hafen" >
+                            <img src={KeilHafen} className="hafen" />
                         </div>
-                        <p>
-                            Typefox was founded in 2016 by Sven Efftinge, Mortiz Eysholt and Dr. Jan Koehnlein. They founded Typefox with the mission of creating developer tools for eveyone. The headquarters is located in Kiel, Germany.
+                        <p className="para">
+                            TypeFox was founded in 2016 by Sven Efftinge, Mortiz Eysholdt and Dr. Jan Koehnlein. They founded TypeFox with the mission to remove friction in developer's  life. The headquarters is located in Kiel, Germany.
                         </p>
                     </div>
+                    <br />
                     <div>
                         <div className="img-container">
-                            <img src={KeilImage}/>
+                            <img src={Team} />
                         </div>
                         <p>
-                            We're a team of less than 20 people which has over 10 years experience in language design, IDE and tool development. If you'd like to join the team, have a look at the <Link to='/careers'>careers</Link> page. We're always curious to hear from talented and commited tech lovers
+                            We're a team of less than 20 people which has over 10 years experience in language design, IDE and tool development. If you'd like to join the team, have a look at the <Link to='/careers' style={{ textDecoration: 'underline' }}>career page</Link>. We're always curious to hear from talented and commited tech lovers.
                         </p>
                     </div>
                 </Description>
             </Container>
             <Hightlight>
-                Open-source let us take part in building the future. Let's do it together.
+                Open-source let us build the future together.
             </Hightlight>
             <Container>
-                <h2>Our Journey</h2>
+                <h2 style={{marginTop: 100}}>Our Journey</h2>
                 <Timeline>
                     <div>
                         <div className="date">04/2019</div>
@@ -208,40 +280,44 @@ const AboutPage: React.SFC<{}> = () => (
                     </div>
                     <div>
                         <div className="date">03/2017</div>
-                        <div className="description">Creation of Theia <span>| open-source IDE in cooperation with Erricson</span></div>
+                        <div className="description">Creation of Theia <span>| open-source IDE in cooperation with Ericsson</span></div>
                     </div>
-                    <div>
+                    <div className="since">
+                        <div className="date"><span>00/</span>2016</div>
+                        <div className="description">Development of Xtext, Sprotty &amp; Language Service Protocol (LSP)</div>
+                    </div>
+                    <div className="last">
                         <div className="date">01/2016</div>
-                        <div className="description">Founding Typefox <span>| tool development, language design, consulting</span></div>
+                        <div className="description">Founding TypeFox <span>| tool development, language design, consulting</span></div>
                     </div>
                 </Timeline>
             </Container>
             <Container>
-                <h2>Our Partners</h2>
+                <h2 style={{marginBottom: 40}}>Our Partners</h2>
                 <Partners>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={ARM} alt="ARM Logo" />
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={Bosch} alt="Bosch Logo" style={{transform: 'scale(2) translateY(-5px)'}} />
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={Eclipse} alt="Eclipse Logo" style={{transform: 'scale(1.6)'}}/>
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={Ericsson} alt="Ericsson Logo" style={{transform: 'scale(2)'}}/>
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={GoogleCloud} alt="Google Cloud Logo" style={{transform: 'scale(2.2)'}}/>
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={Redhat} alt="Redhat Logo" style={{transform: 'scale(1.5)'}}/>
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={Sap} alt="SAP Logo"/>
                     </div>
                     <div className='img-container'>
-                        <img src={IBM} alt="IBM Logo"/>
+                        <img src={IBM} alt="IBM Logo" style={{transform: 'scale(1.4)'}}/>
                     </div>
                 </Partners>
             </Container>
