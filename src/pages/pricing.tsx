088725267bfa772634c50bc6cecd29f01aa3e372
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-11-25 12:41:33 +0000

    Add "For Students" & "Business Solutions" sections.

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled'
 import IndexLayout from '../layouts'
 import { plans } from '../utils/plans'
 import PricingBox from '../components/PricingBox'
+import { Link } from 'gatsby'
 import { sizes } from '../styles/variables'
 
 
@@ -24,6 +25,42 @@ const StyledPricingPage = styled.div`
             @media(max-width: 500px) {
                 flex-direction: column;
                 align-items: center;
+            }
+        }
+    }
+
+    /* ------------------------------------------- */
+    /* ----- Section Links ----- */
+    /* ------------------------------------------- */
+
+    .links {
+        display: flex;
+        justify-content: space-between;
+
+        @media(max-width: ${sizes.breakpoints.md}) {
+            flex-direction: column;
+        }
+
+        & > div {
+
+            &:not(:last-of-type) {
+                margin-bottom: 4rem;
+            }
+
+            h2 {
+                margin-bottom: 3rem;
+            }
+
+            p:last-of-type {
+                margin-bottom: 3rem;
+            }
+
+            .btn {
+                margin-bottom: 2rem;
+            }
+
+            @media(min-width: ${sizes.breakpoints.md}) {
+                width: 40%;
             }
         }
     }
@@ -52,6 +89,24 @@ const PricingPage: React.SFC<{}> = () => (
                                     features={plan.features}
                                 />
                             )}
+                        </div>
+                    </section>
+
+                    {/* ----- Section Links ----- */}
+
+                    <section className="links">
+                        <div>
+                            <h2>For Students</h2>
+                            <p>Students get the unlimited subscription for $9.</p>
+                            <p>Make sure your student email is set as your primary email on GitHub. And please let us know if your educational institution is not correctly recognized.</p>
+                            <a href="#" className="btn">I’m a Student</a>
+                        </div>
+                        <div>
+                            <h2>Business Solutions</h2>
+                            <p>Here you can find further information for your business solution including its pricing.</p>
+                            <Link to="/education" className="btn">Education</Link>
+                            <Link to="/recruiting" className="btn">Recruiting</Link>
+                            <Link to="/vendor" className="btn">Vendor</Link>
                         </div>
                     </section>
 
