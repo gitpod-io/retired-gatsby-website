42abdcc7625d72485d9eb03883e6fefd126ebcbb
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-11-28 11:01:28 +0000

    Make Nav responsive for small responsive for small devices.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,16 +5,19 @@ import { Link } from 'gatsby'
 import GitpodLogoDark from '../resources/gitpod-logo-dark.svg'
 import { colors } from '../styles/variables'
 import DropDown from '../components/DropDown'
-
+import Hamburger from '../resources/hamburger.svg'
+import Multiply from '../resources/multiply.svg'
 
 const StyledNav = styled.nav`
     background: ${colors.offWhite};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4rem 0;
 
-    & > div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 4rem 0;
+    @media (max-width: 980px) {
+        display: block;
+        font-size: 120%;
     }
 
     img {
@@ -24,11 +27,34 @@ const StyledNav = styled.nav`
     ul {
         display: flex;
         align-items: center;
+
+        @media(max-width: 980px) {
+            position: absolute;
+            top: 18%;
+            left: 0;
+            flex-direction: column;
+            width: 100vw;
+            height: 85vh;
+            text-align: center;
+            justify-content: center;
+            align-items: center;
+            background: ${colors.offWhite};
+            z-index: 1;
+        }
     }
 
     li {
         &:not(:last-child) {
             margin-right: 5rem;
+        }
+
+        @media(max-width: 980px) {
+            width: 100%;
+
+            &:not(:last-child) {
+                margin: 0;
+                margin-bottom: 3rem;
+            }
         }
     }
 
@@ -58,34 +84,110 @@ const StyledNav = styled.nav`
             }
         }
     }
+
+    .nav__btn {
+        border: none;
+        z-index: 10000;
+        background: ${colors.offWhite};
+
+        &:hover {
+            background: ${colors.offWhite};
+        }
+
+        &-container {
+            position: absolute;
+            top: 4rem;
+            right: 2rem;
+        }
+
+        img {
+            height: 4rem;
+        }
+
+        @media(min-width: 980px) {
+            display: none;
+        }
+    }
+
 `
 
-const Nav: React.SFC<{}> = () => (
-    <StyledNav role="navigation">
-        <div className="row">
-            <Link to="/"><img alt="Gitpod Logo" src={GitpodLogoDark} /></Link>
-            <ul>
-                <li><Link to='/features' className="link">Features</Link></li>
-                <li><Link to='/pricing' className="link">Pricing</Link></li>
-                <li><Link to='/enterprise' className="link">Enterprise</Link></li>
-                <li>
-                    <DropDown title="Solutions">
-                        <li><Link to='/education' className="link">Education</Link></li>
-                        <li><Link to='/recruiting' className="link">Recruiting</Link></li>
-                        <li><Link to='/vendor' className="link">Vendor</Link></li>
-                    </DropDown>
-                </li>
-                <li>
-                    <DropDown title="Resources">
-                        <li><Link to='/docs' className="link">Docs</Link></li>
-                        <li><Link to='/blog' className="link">Blog</Link></li>
-                        <li><a href="https://spectrum.chat/gitpod/" target="_blank" className="link">Community</a></li>
-                    </DropDown>
-                </li>
-                <li><Link to="/#get-started" className="btn btn--cta">Start for Free</Link></li>
-            </ul>
-        </div>
-    </StyledNav>
-)
+class Nav extends React.Component {
+
+    state = {
+        isNavRendered: false
+    }
+
+    handleResize = () => {
+        if (window.innerWidth < 980) {
+            this.setState({ isNavRendered: false })
+        } else {
+            this.setState({ isNavRendered: true })
+        }
+    }
+
+    toggleNavigation = () => {
+        this.setState({ isNavRendered: !this.state.isNavRendered })
+    }
+
+    event = () => {
+        if (window.innerWidth <= 980) {
+            this.toggleNavigation()
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize)
+        if (window.innerWidth >= 980) {
+            this.toggleNavigation()
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="grey-container">
+                <div className="row">
+                    <StyledNav role="navigation" className="nav">
+                            <Link to="/"><img alt="Gitpod Logo" src={GitpodLogoDark} /></Link>
+                            <div className="nav__btn-container">
+                                <button
+                                    className="nav__btn"
+                                    aria-label="Navigation Toggle"
+                                    onClick={this.toggleNavigation}
+                                >
+                                    {this.state.isNavRendered ? <img src={Multiply} alt="close menu icon" /> : <img src={Hamburger} alt="hamburger menu icon" />}
+                                </button>
+                            </div>
+                            {
+                                this.state.isNavRendered && (
+                                <ul>
+                                    <li><Link to='/features' className="link">Features</Link></li>
+                                    <li><Link to='/pricing' className="link">Pricing</Link></li>
+                                    <li><Link to='/enterprise' className="link">Enterprise</Link></li>
+                                    <li>
+                                        <DropDown title="Solutions">
+                                            <li><Link to='/education' className="link">Education</Link></li>
+                                            <li><Link to='/recruiting' className="link">Recruiting</Link></li>
+                                            <li><Link to='/vendor' className="link">Vendor</Link></li>
+                                        </DropDown>
+                                    </li>
+                                    <li>
+                                        <DropDown title="Resources">
+                                            <li><Link to='/docs' className="link">Docs</Link></li>
+                                            <li><Link to='/blog' className="link">Blog</Link></li>
+                                            <li><a href="https://spectrum.chat/gitpod/" target="_blank" className="link">Community</a></li>
+                                        </DropDown>
+                                    </li>
+                                    <li><Link to="/#get-started" className="btn btn--cta">Start for Free</Link></li>
+                                </ul>
+                                )
+                            }
+                    </StyledNav>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 export default Nav
