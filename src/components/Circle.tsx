51987246af666054a6c7ceeb32e255d0cf825510
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-11-29 05:44:29 +0000

    made the circle smaller.

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Circle: React.SFC<{}> = () => (
-  <svg width={20} height={20} fill="none">
-    <circle cx={10} cy={10} r={10} fill="#0087BE" />
+  <svg width={18} height={18} fill="none">
+    <circle cx={8} cy={8} r={8} fill="#0087BE" />
   </svg>
 )
 
