65be5d44a165c4ac604987253787bd2098a0f583
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-12-04 06:27:36 +0000

    Made everything a bit smaller.

--- src/styles/normalize.ts
+++ src/styles/normalize.ts
@@ -14,7 +14,7 @@ export default `
     }
 
     html {
-        font-size: 62.5%; // 10 / 16 = 0.625 * 100 = 62.5 = 10px
+        font-size: 59.5%; // 10 / 16 = 0.625 * 100 = 62.5 = 10px
 
         @media(max-width: ${sizes.breakpoints.md}) {
             font-size: 56.5%;
