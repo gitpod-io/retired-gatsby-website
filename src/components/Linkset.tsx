86c6717b8b8e14a7186fe89ebc650c73cb5720fa
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-12-10 10:51:14 +0000

    small updates.

--- src/components/Linkset.tsx
+++ src/components/Linkset.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { colors } from '../styles/variables'
 import { Link } from 'gatsby'
-import Arrow from '../resources/arrow.svg'
+// import Arrow from '../resources/arrow.svg'
 import DownArrow from '../resources/arrow-down.svg'
 
 const StyledLinkSet = styled.div`
@@ -17,24 +17,25 @@ const StyledLinkSet = styled.div`
         font-size: 1.5rem;
 
         &.caption {
+            margin-bottom: .4rem;
             border: none;
-            font-weight: 600;
+            font-weight: 400;
             font-size: 1.9rem;
 
             img {
                 display: inline-block;
-                margin: -2rem 1rem 0 0;
+                margin: 0 1rem 0 0;
             }
         }
 
         &:not(.caption) {
-            padding-left: 2rem;
+            padding-left: 3rem;
         }
     }
 
     li {
         &:not(.caption) {
-            padding-left: 2rem;
+            padding-left: 1rem;
         }
 
         a {
@@ -73,10 +74,7 @@ class Linkset extends React.Component<LinkSetProps, {}> {
                         onClick={this.handleClick}
                         className="caption"
                     >
-                        { isRendered ?
-                            <img alt=""  src={Arrow}/>
-                                :
-                            <img alt="" src={DownArrow}/> } {caption}
+                        <img alt="" src={DownArrow}/> {caption}
                     </Link>
                 </li>
                 {
