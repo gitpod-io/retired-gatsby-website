36ea7ada98b0c0270fdb5e07302acdfa95b5954d
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-12-12 11:56:09 +0000

    Remove scroll-snap as it does not seem to work on IOS.

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -19,9 +19,6 @@ const StyledTestimonials = styled.div`
     .tweets {
         display: flex;
         align-items: center;
-        overflow-x: auto;
-        scroll-padding: 0 50%;
-        scroll-snap-type: x;
 
         @media(min-width: ${sizes.breakpoints.md}) {
             padding-left: calc((100% - ${sizes.grid.maxWidth})/2);
@@ -45,7 +42,6 @@ const StyledTestimonials = styled.div`
     }
 
     .tweet {
-        scroll-snap-align: center;
         max-width: 100%;
 
         &:not(:last-of-type) {
