80125ec6e33456628e013069722a91277cb0e58e
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-12-17 13:36:23 +0000

    [documentation] change edit pen color to blue on hover.

--- src/templates/doc.tsx
+++ src/templates/doc.tsx
@@ -6,7 +6,6 @@ import { colors } from '../styles/variables'
 import DownArrow from '../resources/arrow-down.svg'
 import { Link } from 'gatsby'
 import DocsLayout from '../layouts/docs'
-import Pen from '../resources/pen.svg'
 
 interface DocTemplateProps {
     data: {
@@ -47,7 +46,7 @@ const DocTemplate: React.SFC<DocTemplateProps> = ({ data }) => {
                         <h4 style={{ color: colors.textLight, marginBottom: 0, marginTop: 30 }}>Docs</h4>
                         <div title="Edit this page in Gitpod" className="pen-container">
                             <a href={editUrl} aria-label="Edit Button" target="_blank">
-                                <img alt="Edit Pen" src={Pen}/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22.454" height="25.307" viewBox="0 0 22.454 25.307"><title>Edit Button</title><path d="M25.088,13.816,12.516,27.982h0L8.063,22.964,20.635,8.8l4.453,5.018Zm.787-.887L27.8,10.763a2.744,2.744,0,0,0,.011-3.55l-1.313-1.48a2.048,2.048,0,0,0-3.151.012L21.422,7.911l4.453,5.018ZM11.729,28.869l-.163.184L6,30.307l1.113-6.272.163-.184,4.453,5.018Z" transform="translate(-6 -5) translate(6 5) translate(-6 -5)"/></svg>
                             </a>
                         </div>
                         <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
