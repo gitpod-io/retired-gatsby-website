8427cb0c3b558c43ac48c6fc9154dd01eae61269
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-01-20 14:36:31 +0000

    [pricing] "For Students" rewords + fix pricing.

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -226,7 +226,7 @@ const PricingPage: React.SFC<{}> = () => (
                         </div>
                         <div>
                             <h2>For Students</h2>
-                            <p>Students get the <strong>professional subscription for $9</strong> per month. Make sure your student email is set as your primary email on GitHub.</p>
+                            <p>Students get the <strong>unlimited subscription for {isEurope() ? '€8' : '$9'}</strong> per month. Make sure your student email is set as your primary email on GitHub.</p>
                             <a href="/contact/" className="btn">Doesn't Work?</a>
                         </div>
                     </section>
