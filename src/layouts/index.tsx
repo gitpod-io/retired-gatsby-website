9c4cb4b07cde725b5658065f783c046f1bb0d1b2
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-01-23 11:28:46 +0000

    [accessibility] remove the ugly foucs onClick & and have it too... for keyboard users.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -23,75 +23,102 @@ type StaticQueryProps = {
   }
 }
 
-const IndexLayout: React.SFC<{ title?: string, canonical?: string }> = ({ title, canonical, children }) => (
-  <StaticQuery
-    query={graphql`
-      query IndexLayoutQuery {
-        site {
-          siteMetadata {
-            title
-            description
-            siteUrl
-          }
+class IndexLayout extends React.Component<{ title?: string, canonical?: string }, {}> {
+
+    handleFirstTab = (e :any) => {
+        if (e.keyCode === 9) { // the "I am a keyboard user" key
+            document.body.classList.add('user-is-tabbing');
+            console.log(document.body.classList)
+            window.removeEventListener('keydown', this.handleFirstTab);
+            window.addEventListener('mousedown', this.handleMouseDownOnce);
         }
-      }
-    `}
-    render={(data: StaticQueryProps) => (
-      <LayoutRoot>
-        <Helmet>
-          <html lang="en" />
-          <title>{title && `${title} - Gitpod` || data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          <meta name="keywords" content="cloud ide, github ide, gitlab ide, javascript, online ide, web ide, code review" />
-          {
-            canonical ? <link rel="canonical" href={`${data.site.siteMetadata.siteUrl}${canonical}`} /> : null
-          }
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet" />
-          <meta name="theme-color" content={colors.white} />
-          <link rel="apple-touch-icon" type="image/png" href={GitpodIconApple} sizes="180x180" />
-          <link rel="icon" type="image/png" href={GitpodIcon196} sizes="196x196" />
-          <link rel="icon" type="image/svg+xml" href={GitpodIcon} sizes="any" />
+    }
 
-          <meta name="twitter:card" content="summary"></meta>
-          <meta name="twitter:site" content="@gitpod"></meta>
-          <meta name="twitter:creator" content="@gitpod"></meta>
+    handleMouseDownOnce = () => {
+        document.body.classList.remove('user-is-tabbing');
+        
+        window.removeEventListener('mousedown', this.handleMouseDownOnce);
+        window.addEventListener('keydown', this.handleFirstTab);
+    }
 
-          <meta property="og:url" content={data.site.siteMetadata.siteUrl} />
-          <meta property="og:title" content={title || data.site.siteMetadata.title} />
-          <meta property="og:description" content={data.site.siteMetadata.description} />
-          <meta property="og:image" content="https://www.gitpod.io/media-image.jpg" />
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleFirstTab);
+    }
 
-          <meta name="google-site-verification" content="NBio3hCkfn2FKJpqZritJpXuyKo54noPGZzWsjDIp-M" />
-        </Helmet>
-        <Nav />
-        <LayoutMain>
-            <CookieConsent buttonClasses="primary"
-                containerClasses="consent"
-                style={{
-                    backgroundColor: colors.white,
-                    color: colors.text,
-                    borderTop: borders.light,
-                }}
-                contentStyle={{
-                    backgroundColor: 'transparent',
-                    margin: '10px 15px'
-                }}
-                buttonStyle={{
-                    backgroundColor: 'transparent',
-                    border: 'solid 1px '+colors.text,
-                    borderRadius: '100px',
-                    padding: '.8rem 2.2rem',
-                    margin: '10px 15px'
-                }}
-            >
-                This website uses cookies to<br /> enhance the user experience.<br /> Read our <Link style={{color: '#35C9FF'}} to="/privacy/">privacy policy</Link> for more info.
-            </CookieConsent>
-          {children}
-        </LayoutMain>
-        <Footer />
-      </LayoutRoot>
-    )}
-  />
-)
+    render() {
+        const { title, canonical, children } = this.props
 
+        return (
+            <StaticQuery
+                query={graphql`
+                query IndexLayoutQuery {
+                    site {
+                    siteMetadata {
+                        title
+                        description
+                        siteUrl
+                    }
+                    }
+                }
+                `}
+                render={(data: StaticQueryProps) => (
+                <LayoutRoot>
+                    <Helmet>
+                    <html lang="en" />
+                    <title>{title && `${title} - Gitpod` || data.site.siteMetadata.title}</title>
+                    <meta name="description" content={data.site.siteMetadata.description} />
+                    <meta name="keywords" content="cloud ide, github ide, gitlab ide, javascript, online ide, web ide, code review" />
+                    {
+                        canonical ? <link rel="canonical" href={`${data.site.siteMetadata.siteUrl}${canonical}`} /> : null
+                    }
+                    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet" />
+                    <meta name="theme-color" content={colors.white} />
+                    <link rel="apple-touch-icon" type="image/png" href={GitpodIconApple} sizes="180x180" />
+                    <link rel="icon" type="image/png" href={GitpodIcon196} sizes="196x196" />
+                    <link rel="icon" type="image/svg+xml" href={GitpodIcon} sizes="any" />
+
+                    <meta name="twitter:card" content="summary"></meta>
+                    <meta name="twitter:site" content="@gitpod"></meta>
+                    <meta name="twitter:creator" content="@gitpod"></meta>
+
+                    <meta property="og:url" content={data.site.siteMetadata.siteUrl} />
+                    <meta property="og:title" content={title || data.site.siteMetadata.title} />
+                    <meta property="og:description" content={data.site.siteMetadata.description} />
+                    <meta property="og:image" content="https://www.gitpod.io/media-image.jpg" />
+
+                    <meta name="google-site-verification" content="NBio3hCkfn2FKJpqZritJpXuyKo54noPGZzWsjDIp-M" />
+                    </Helmet>
+                    <Nav />
+                    <LayoutMain>
+                        <CookieConsent buttonClasses="primary"
+                            containerClasses="consent"
+                            style={{
+                                backgroundColor: colors.white,
+                                color: colors.text,
+                                borderTop: borders.light,
+                            }}
+                            contentStyle={{
+                                backgroundColor: 'transparent',
+                                margin: '10px 15px'
+                            }}
+                            buttonStyle={{
+                                backgroundColor: 'transparent',
+                                border: 'solid 1px '+colors.text,
+                                borderRadius: '100px',
+                                padding: '.8rem 2.2rem',
+                                margin: '10px 15px'
+                            }}
+                        >
+                            This website uses cookies to<br /> enhance the user experience.<br /> Read our <Link style={{color: '#35C9FF'}} to="/privacy/">privacy policy</Link> for more info.
+                        </CookieConsent>
+                    {children}
+                    </LayoutMain>
+                    <Footer />
+                </LayoutRoot>
+                )}
+            />
+        )
+    }
+ 
+}
 export default IndexLayout
