f889857406be0e42644e4d7c54ac764955012cf4
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-03-09 07:39:20 +0000

    [github-teacher-toolbox] adjust the quote.

--- src/pages/github-teacher-toolbox.tsx
+++ src/pages/github-teacher-toolbox.tsx
@@ -20,35 +20,35 @@ import Offers from '../components/Offers'
 const offers: PricingBoxProps[] = [
     {
         title: 'Open-Source',
-        img: <object role="presentation" tabIndex={-1} data={IconOpenSource}/>,
+        img: <object role="presentation" tabIndex={-1} data={IconOpenSource} />,
         price: 'Free',
         duration: '50 hours / month',
         hideButton: true,
-        features: ['Public Repos', <span className="span"> &lt; 4 Parallel Workspaces <PopOver description="The number of workspaces running at the same time."/></span>],        
+        features: ['Public Repos', <span className="span"> &lt; 4 Parallel Workspaces <PopOver description="The number of workspaces running at the same time." /></span>],
         text: 'Free for everyone'
     },
     {
         title: 'Teacher',
-        img: <object role="presentation" tabIndex={-1} data={Owl}/>,
-        price: <><span style={{textDecoration: 'line-through', opacity: .8}}>{(isEurope() ? '€8' : '$9')}</span> Free</>,
+        img: <object role="presentation" tabIndex={-1} data={Owl} />,
+        price: <><span style={{ textDecoration: 'line-through', opacity: .8 }}>{(isEurope() ? '€8' : '$9')}</span> Free</>,
         duration: '100 hours / month',
         hideButton: true,
-        features: ['Private & Public Repos', <span className="span">4 Parallel Workspaces <PopOver description="The number of workspaces running at the same time."/></span>, <span className="span">30min Timeout <PopOver description="Workspaces without user activity are stopped after 30 minutes."/></span>, 'Limited to 6 months'],
+        features: ['Private & Public Repos', <span className="span">4 Parallel Workspaces <PopOver description="The number of workspaces running at the same time." /></span>, <span className="span">30min Timeout <PopOver description="Workspaces without user activity are stopped after 30 minutes." /></span>, 'Limited to 6 months'],
         transform: 'scale(1.05)',
-        btn: <a 
-                href="https://gitpod.io/subscription/" 
-                target="_blank" 
-                className="btn btn--cta"
-                style={{color: colors.white}}
-             >
-                Claim Offer
+        btn: <a
+            href="https://gitpod.io/subscription/"
+            target="_blank"
+            className="btn btn--cta"
+            style={{ color: colors.white }}
+        >
+            Claim Offer
              </a>,
         banner: 'Exclusive Offer for the GitHub Teacher Toolbox',
         bannerColor: '#EAA42F'
     },
     {
         title: 'Gitpod Education',
-        img: <object role="presentation" tabIndex={-1} data={Saturn}/>,
+        img: <object role="presentation" tabIndex={-1} data={Saturn} />,
         price: <>From {(isEurope() ? '€0.8' : '$0.9')}</>,
         duration: 'Per user/month',
         feature: <Link to="/education/">Learn More</Link>,
@@ -59,47 +59,47 @@ const offers: PricingBoxProps[] = [
 
 const GithubTeacherToolBoxPage: React.SFC<{}> = () => (
     <IndexLayout canonical="/github-teacher-toolbox/" title="GitHub Teacher Toolbox" description="With Gitpod you can create coding exams and exercises easily, help out your students via live tutoring, and benefit from a simple onboarding with any device.">
-            <div className="grey-container">
+        <div className="grey-container">
 
-                {/* ----- Banner ----- */}  
+            {/* ----- Banner ----- */}
 
-                <Banner 
-                    subtitle="GitHub Teacher Toolbox"
-                    title={<h1>Let’s Focus on Teaching Code, not Tedious Setups!</h1>}
-                    paragraph={<span>With Gitpod you can create coding exams and exercises easily, help out your students via live tutoring, and benefit from a simple onboarding with any device. <Link to="/education">Learn more</Link></span>}
-                    linkPath="https://gitpod.io/subscription/"
-                    linkText="Claim Offer"
-                    img={<img src={TeacherBox} alt="GitHub Teacher Toolbox" />}
-                />
+            <Banner
+                subtitle="GitHub Teacher Toolbox"
+                title={<h1>Let’s Focus on Teaching Code, not Tedious Setups!</h1>}
+                paragraph={<span>With Gitpod you can create coding exams and exercises easily, help out your students via live tutoring, and benefit from a simple onboarding with any device. <Link to="/education">Learn more</Link></span>}
+                linkPath="https://gitpod.io/subscription/"
+                linkText="Claim Offer"
+                img={<img src={TeacherBox} alt="GitHub Teacher Toolbox" />}
+            />
 
-                <Offers 
-                    title="GitHub Teacher Toolbox"
-                    para={
-                        <p>With the <a href="https://education.github.com/teachers" target="_blank">GitHub Teacher Toolbox</a>, you get the same free plan as your students with the <a href="https://education.github.com/students" target="blank">GitHub Student Developer Pack</a>.
+            <Offers
+                title="GitHub Teacher Toolbox"
+                para={
+                    <p>With the <a href="https://education.github.com/teachers" target="_blank">GitHub Teacher Toolbox</a>, you get the same free plan as your students with the <a href="https://education.github.com/students" target="blank">GitHub Student Developer Pack</a>.
                         <br />
                         We’re happy to support educators teaching frictionless coding.</p>
-                    }
-                    offers={offers}
-                />
-            </div>
+                }
+                offers={offers}
+            />
+        </div>
 
-            <div className="row">
+        <div className="row">
 
-                {/* ----- Onboarding ---- */}
+            {/* ----- Onboarding ---- */}
 
-                <OnBoarding link="https://calendly.com/gitpod/onboarding?back=1&month=2020-02" />
+            <OnBoarding link="https://calendly.com/gitpod/onboarding?back=1&month=2020-02" />
 
-                {/* ----- Quote ----- */}                            
+            {/* ----- Quote ----- */}
 
-                <Quote 
-                    title="Stop wasting time with tedious setups, learn amazing programming skills instead!"                    
-                />
+            <Quote
+                title="Stop wasting time with tedious setups, teach amazing programming skills instead!"
+            />
 
-                {/* ----- Info ----- */}  
+            {/* ----- Info ----- */}
 
-                <InfoCard />
-                
-            </div>
+            <InfoCard />
+
+        </div>
     </IndexLayout>
 )
 
