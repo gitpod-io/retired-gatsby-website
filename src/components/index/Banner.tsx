b72893f2436b3cee20145a0c6e0f776315110ca1
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-04-01 12:44:19 +0000

    [index] Update Banner Screenshot.

--- src/components/index/Banner.tsx
+++ src/components/index/Banner.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import { Link } from 'gatsby'
-import ReflectionScreenshot from '../../resources/reflection-screenshot.png'
-import ReflectionScreenshotPrespectiveLess from '../../resources/reflection-screenshot--1.png'
+import Screenshot from '../../resources/screenshot.png'
 import styled from '@emotion/styled'
 import { colors, sizes } from '../../styles/variables'
 
@@ -59,48 +58,17 @@ const Styled = styled.div`
         &__screenshot {
             display: block;
             width: 100%;
+            box-shadow: 0 2rem 6rem rgba(0,0,0, .2);
 
             @media(max-width: ${sizes.breakpoints.lg}) {
                 width: 100%;
             }
 
             @media(min-width: ${sizes.breakpoints.lg}) {
+                transform: scale(1.18) translate(7rem, 6rem);
 
                 &-container {
                     width: 50%;
-                }
-            }
-
-            &--1 {
-                transform: scale(1.3) translate(6rem, 6rem);
-
-                @media(max-width: ${sizes.breakpoints.lg}) {
-                    display: none;
-                }
-            }
-
-            &--2 {
-                margin-top: 10rem;
-                transform: scale(1.3) translateX(1.5rem);
-
-                @media(max-width: 740px) {
-                    transform: scale(1.3) translateX(1.2rem);                    
-                }
-
-                @media(max-width: 580px) {
-                    transform: scale(1.3) translateX(1rem);                    
-                }
-
-                @media(max-width: 480px) {
-                    transform: scale(1.3) translateX(.9rem);                    
-                }
-
-                @media(max-width: 440px) {
-                    transform: scale(1.3) translateX(.8rem);                    
-                }
-
-                @media(min-width: ${sizes.breakpoints.lg}) {
-                    display: none;
                 }
             }
         }
@@ -138,7 +106,7 @@ const Banner = () => (
                     <div className="banner__text-box">
                         <h1>
                             Are You <br />Ready-To-Code?
-                                <span>Start Instantly. Anywhere.</span>
+                            <span>Start Instantly. Anywhere.</span>
                         </h1>
                         <p>Gitpod launches ready-to-code dev environments<br /> for your GitHub or GitLab project with a single click.</p>
                         <div className="btn-wrapper">
@@ -148,8 +116,7 @@ const Banner = () => (
                         </div>
                     </div>
                     <div className="banner__screenshot-container">
-                        <img alt="Gitpod Screenshot" src={ReflectionScreenshot} className="banner__screenshot banner__screenshot--1" />
-                        <img alt="Gitpod Screenshot" src={ReflectionScreenshotPrespectiveLess} className="banner__screenshot banner__screenshot--2" />
+                        <img alt="Gitpod Screenshot" src={Screenshot} className="banner__screenshot" />
                     </div>
                 </header>
             </div>
