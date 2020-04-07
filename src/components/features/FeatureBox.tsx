ba7043e50269ee92ee3ad460bad3be18f37884b9
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-04-07 11:17:31 +0000

    [features] make intro boxes smaller.

--- src/components/features/FeatureBox.tsx
+++ src/components/features/FeatureBox.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
 
 import styled from '@emotion/styled'
-import { colors, shadows } from '../../styles/variables'
+import { colors, shadows, sizes } from '../../styles/variables'
 
 const StyledFeatureBox = styled.div`
-    min-width: 20rem;
-    padding: 1.5rem .8rem;
+    position: relative;
+    padding: 1rem .6rem;
     text-align: center;
     background: ${colors.white};
     box-shadow: ${shadows.light};
     border-radius: 2rem;
-    height: 150px;
-    position:relative;
+    height: 120px;
+
+    @media(max-width: 980px) {
+       height: 100px;
+    }
 
     h3 {
         font-weight: 400;
-        font-size: 2rem;
+        font-size: 1.7rem;
+
+        @media(max-width: 980px) {
+            font-size: 1.5rem;
+        }
     }
 
     img {
-        height: 4rem;
+        height: 3.3rem;
         display: block;
-        margin: 1rem auto;
+        margin: .5rem auto 1rem;
+
+        @media(max-width: ${sizes.breakpoints.sm}) {
+            height: 3rem;
+        }
+
+        @media(max-width: 980px) {
+            height: 2.4rem;
+        }
     }
 
     &:hover {
