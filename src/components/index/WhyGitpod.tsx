ff829576287a5c0b70ed28078be1d3466f616671
Sean Hellum <seanhellum45@gmail.com>
2020-05-05 10:40:02 -0500

    [docs] Fix some more grammar mistakes (#609)

--- src/components/index/WhyGitpod.tsx
+++ src/components/index/WhyGitpod.tsx
@@ -241,9 +241,9 @@ const WhyGitpod = () => (
                         </div>
                         <div className="why-gitpod__text">
                             <h3>Seamless GitHub & GitLab Integration</h3>
-                            <p>Gitpod let’s you stay in the flow by blending perfectly into GitHub and GitLab. Start your workflow from there and let Gitpod beam you in a ready-to-code dev environment.</p>
-                            <p>No matter you want to fix a bug, add a feature or do a code review, Gitpod get's you started immediately.</p>
-                            <p>Read more about <a href="/docs/context-urls/">Integration.</a></p>
+                            <p>Gitpod lets you stay in the flow by blending perfectly into GitHub and GitLab. Start your workflow from there and let Gitpod beam you in a ready-to-code dev environment.</p>
+                            <p>Whether you want to fix a bug, add a feature or do a code review, Gitpod gets you started immediately.</p>
+                            <p>Read more about supported <a href="/docs/context-urls/">context URLs</a>.</p>
                         </div>
                     </div>
                 </div>
@@ -258,8 +258,8 @@ const WhyGitpod = () => (
                         </div>
                         <div className="why-gitpod__text">
                             <h3>Based on the Online IDE Theia </h3>
-                            <p>Gitpod is not just another online playground, but a full-featured dev environment, just like your local IDE.</p>
-                            <p>It includes fully-functional <strong>Linux terminals</strong>, you can install any <strong>VS Code extensions</strong> and provides all the other good features provided by <strong>Theia</strong>, the vendor neutral and highly extensible VS Code alternative, we have developed.</p>
+                            <p>Gitpod isn't just an online playground, but a full-featured dev environment, just like your local IDE and Terminal.</p>
+                            <p>It includes fully-functional Linux-based <strong>Terminals</strong>, and you can install any <strong>VS Code extensions</strong> or enjoy many other great features from <strong>Theia</strong>, the vendor neutral and highly extensible IDE framework we have developed.</p>
                             <p>Read more about <a href="/docs/ide/">Theia</a>.</p>
                         </div>
                     </div>
