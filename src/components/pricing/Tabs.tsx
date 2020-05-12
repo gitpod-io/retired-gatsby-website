95b3f5d5ce0c0b2cd8aa9ac7b7b9f183a51f1c2a
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-05-12 11:47:24 +0000

    [Tabs] make the corners rounded + add hover effect.

--- src/components/pricing/Tabs.tsx
+++ src/components/pricing/Tabs.tsx
@@ -4,30 +4,37 @@ import styled from '@emotion/styled'
 import { colors } from '../../styles/variables'
 import { PricingBoxesProps } from './PricingBoxes'
 
-const Tab = styled.button`
+const Tab = styled.button<{ isRendered: boolean }>`
     display: block;
     padding: 1.3rem 2rem;
     font-size: 18px;
     color: ${colors.textLight};
     min-width: 15rem;
     text-align: center;
-    background-color: ${colors.white};
     border: 1px solid ${colors.offWhite2};
+    border-radius: 6px 6px 0 0;
     border-bottom-color: transparent;
     margin-bottom: -1px;
     cursor: pointer;
+    transition: all .1s;
 
     &:first-of-type {
+        background-color: ${ ({ isRendered }) => isRendered ? colors.offWhite2 : colors.white };
        border-right: none;
     }
 
     &:last-of-type {
+        background-color: ${ ({ isRendered }) => isRendered ? colors.white : colors.offWhite2 };
         border-left: none;
     }
 
     body.user-is-tabbing &:focus {
         outline: none;
         border: 1px solid #1AA6E4;;
+    }
+
+    &:hover {
+        color: ${colors.link};
     }
 `
 
@@ -36,16 +43,18 @@ const Tabs = ({ isRendered, changeIsRendered }: PricingBoxesProps) => (
         display: 'flex',
         justifyContent: 'center'
     }}>
-        <Tab style={{
-            backgroundColor: isRendered ? colors.offWhite2 : colors.white,
-        }}
+        <Tab 
+            isRendered={isRendered}
             onClick={() => changeIsRendered(false)}
-        >Cloud</Tab>
-        <Tab style={{
-            backgroundColor: isRendered ? colors.white : colors.offWhite2,
-        }}
+        >
+            Cloud
+        </Tab>
+        <Tab 
+            isRendered={isRendered}
             onClick={() => changeIsRendered(true)}
-        >Self-Hosted</Tab>
+        >
+            Self-Hosted
+        </Tab>
     </div>
 )
 
