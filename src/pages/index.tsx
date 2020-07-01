14583ddedd2e34c26a6e1e070e26a06e225bfc9a
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-01 20:18:57 +0500

    Update src/pages/index.tsx
    Co-authored-by: Jan Keromnes <jan.keromnes@typefox.io>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -148,7 +148,7 @@ const IndexPage: React.SFC<{}> = () => (
             path={HighlySecureImg}
             alt="Secure"
             title="Secure and Flight-Proven"
-            text={<><p>Designed with modern security principles and strong encryption at every level, Gitpod keeps your code and your data safe no matter what happens.</p><p>Hardened by professional pentesting and thousands of developer hours every single day.</p></>}
+            text={<><p>Built with modern security principles and strong encryption, Gitpod keeps your code and your data safe no matter what happens.</p><p>Hardened by professional pentesting and thousands of developer hours every single day.</p></>}
         />
 
         <GetStarted />
