c1f2ce9aa2409ebb45119096e451252107aa0c3e
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-15 13:36:08 +0000

    reduce the spacing.

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import styled from '@emotion/styled'
 import FeatureItem, { FeatureItemProps } from './FeatureItem'
+import { sizes } from '../styles/variables'
 
 const StyledFeatures = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    
+    @media(max-width: ${sizes.breakpoints.lg}) {
+        margin-top: -5rem;
+    }
 
     @media(max-width: 900px) {
         flex-direction: column;
