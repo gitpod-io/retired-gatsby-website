32e9d36a90e17c0e92567329dc75aa77fc06ab63
Sven Efftinge <sven.efftinge@typefox.io>
2020-08-05 16:27:33 +0200

    fix enviroments

--- src/components/index/SaveHours.tsx
+++ src/components/index/SaveHours.tsx
@@ -60,7 +60,7 @@ const SaveHours = () => (
                         Save countless of hours of watching your code build.
                 </strong>
                 </h2>
-                <p>With Gitpod you can automate and prebuild your dev enviroments so that you and your team can start coding with a single click.</p>
+                <p>With Gitpod you can automate and prebuild your dev environments so that you and your team can start coding with a single click.</p>
             </div>
         </StyledSaveHours>
     </div>
