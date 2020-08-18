76a805bb264a14d4b4a2558a95858440a1951802
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-08-18 01:38:01 +0000

    add title.

--- src/components/index/TrustedBy.tsx
+++ src/components/index/TrustedBy.tsx
@@ -4,65 +4,69 @@ import styled from '@emotion/styled'
 import { sizes } from '../../styles/variables'
 
 const StyledTrustedBy = styled.section`
-    padding: 3rem 0;
+  padding: 3rem 0;
 
-    .logos {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
+  h2 {
+    text-align: center;
+  }
 
-        @media(max-width: 540px) {
-            display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-        }
+  .logos {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 
-        a {
-            flex-basis: calc(14% - 2.2rem);
-            display: flex;
-            border: 2px solid transparent;
-            justify-content: center;
-            margin: 1.2rem;
-        }
+    @media (max-width: 540px) {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
     }
 
-    .grey-logo {
-        filter: grayscale(100%) contrast(0%) brightness(116%);
-        -webkit-filter: grayscale(100%) contrast(0%) brightness(116%);
+    a {
+      flex-basis: calc(14% - 2.2rem);
+      display: flex;
+      border: 2px solid transparent;
+      justify-content: center;
+      margin: 1.2rem;
+    }
+  }
+
+  .grey-logo {
+    filter: grayscale(100%) contrast(0%) brightness(116%);
+    -webkit-filter: grayscale(100%) contrast(0%) brightness(116%);
+  }
+
+  .fcc {
+    filter: grayscale(100%) contrast(0%);
+    -webkit-filter: grayscale(100%) contrast(0%);
+    width: 13rem;
+
+    @media (max-width: ${sizes.breakpoints.md}) {
+      width: 12rem;
     }
 
-    .fcc {
-        filter: grayscale(100%) contrast(0%);
-        -webkit-filter: grayscale(100%) contrast(0%);
-        width: 13rem;
-
-        @media(max-width: ${sizes.breakpoints.md}) {
-            width: 12rem;
-        }
-
-        @media(max-width: ${sizes.breakpoints.md}) {
-            width: 11rem;
-        }
+    @media (max-width: ${sizes.breakpoints.md}) {
+      width: 11rem;
     }
+  }
 
-    .dwave-logo {
-        transform: scale(1.2);
-        filter: grayscale(100%) contrast(0%) brightness(112%);
-        -webkit-filter: grayscale(100%) contrast(0%) brightness(112%);
-    }
+  .dwave-logo {
+    transform: scale(1.2);
+    filter: grayscale(100%) contrast(0%) brightness(112%);
+    -webkit-filter: grayscale(100%) contrast(0%) brightness(112%);
+  }
 
-    .four-geeks {
-        transform: scale(.9);
-    }
+  .four-geeks {
+    transform: scale(0.9);
+  }
 
-    .gatsby-logo {
-        filter: grayscale(100%) opacity(47%);
-        -webkit-filter: grayscale(100%) opacity(47%);
-    }
+  .gatsby-logo {
+    filter: grayscale(100%) opacity(47%);
+    -webkit-filter: grayscale(100%) opacity(47%);
+  }
 
-    .vue-logo {
-        filter: grayscale(100%) opacity(62%);
-        -webkit-filter: grayscale(100%) opacity(62%);
-    }
+  .vue-logo {
+    filter: grayscale(100%) opacity(62%);
+    -webkit-filter: grayscale(100%) opacity(62%);
+  }
 `
 
 const StyledBrandImage = styled.img<{ transform?: string }>`
@@ -77,42 +81,37 @@ const StyledBrandImage = styled.img<{ transform?: string }>`
     }
 
     @media(min-width: calc(${sizes.breakpoints.lg} + 1px)) {
-        transform: ${ ({ transform }) => transform ? transform : 'none'};
+        transform: ${({ transform }) => (transform ? transform : 'none')};
     }
 `
 
 interface Brand {
-    alt: string
-    url: string
-    svg: string
-    transform?: string,
-    className?: string
+  alt: string
+  url: string
+  svg: string
+  transform?: string
+  className?: string
 }
 
 interface TrustedByProps {
-    brands: Brand[]
+  brands: Brand[]
 }
 
 const TrustedBy = ({ brands }: TrustedByProps) => (
-    <StyledTrustedBy>
-        <div className="row">
-            <div className="logos">
-                {
-                    brands.map((b: Brand) => (
-                        <a href={b.url} target="_blank" className="trustedBy">
-                            <StyledBrandImage
-                                src={b.svg}
-                                alt={b.alt}
-                                transform={b.transform}
-                                className={b.className}
-                            />
-                        </a>
-                    ))
-                }
-            </div>
-        </div>
-    </StyledTrustedBy>
+  <StyledTrustedBy>
+    <div className="row">
+      <h2>
+        Trusted by <strong>Visionary Teams</strong>
+      </h2>
+      <div className="logos">
+        {brands.map((b: Brand) => (
+          <a href={b.url} target="_blank" className="trustedBy">
+            <StyledBrandImage src={b.svg} alt={b.alt} transform={b.transform} className={b.className} />
+          </a>
+        ))}
+      </div>
+    </div>
+  </StyledTrustedBy>
 )
-
 
 export default TrustedBy
