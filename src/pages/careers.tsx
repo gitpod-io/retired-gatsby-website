ec1c803b3ccea05b1c6335ff733543b69a17cbe8
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-01-26 20:52:35 +0000

    make the first sentences bold.

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -160,11 +160,7 @@ const jobs: ExpandableJobProps[] = [
         ],
         textAfterTheLists: '',
         rendered: false,
-<<<<<<< HEAD
         date: '2021-01-26T14:00:00.000Z'
-=======
-        date: '2021-19-01T19:00:00.000Z'
->>>>>>> 69341cb (Fixed dates)
     },
     {
         title: 'Senior Software Engineer (Full-Stack)',
