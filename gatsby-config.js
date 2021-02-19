0382ab638215122c5d6d92513ed38330cfe20a0d
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-02-19 05:07:57 +0000

    [docs]: enable mdx gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,12 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`],
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'blog',
