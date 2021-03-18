b30d7c06344243d20db62977d20da2147c9f1258
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-03-18 13:13:11 +0000

    clean up.

--- src/components/Circle.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Circle: React.SFC<{}> = () => (
-  <svg width={18} height={18} fill="none" style={{transform: 'translateY(.5rem)'}}>
-    <circle cx={8} cy={8} r={8} fill="#0087BE" />
-  </svg>
-)
-
-export default Circle
